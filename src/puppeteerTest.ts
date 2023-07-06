aa2f94b7ea36cee0edba0a1ec0f77db992f61ac7
iinoten <nteten0313@icloud.com>
2023-07-07 02:29:51 +0900

    Add 指定のアカウントのフォロワーが表示されるまでの処理を追加

--- src/puppeteerTest.ts
+++ src/puppeteerTest.ts
@@ -7,8 +7,10 @@ const INSTAGRAM_FIRST_URL = "https://www.instagram.com/"
 const INSTAGRAM_MY_ID = process.env.INSTA_ACCOUNT as string
 const INSTAGRAM_PASSWORD = process.env.INSTA_PASSWORD as string
 
-const waitOptions: {[key: string]: string} = {
-    waitUntil: 'load'
+const DEMO_SEARCH_ID = "iinoten"
+
+const waitOptions: {[key: string]: string[]} = {
+    waitUntil: ['load', 'networkidle2']
 }
 
 export const main = async () => {
@@ -24,11 +26,20 @@ export const main = async () => {
       await page.click('button[type="submit"]'),
       await page.waitForNavigation(waitOptions),
   ])
+  await page.goto(`${INSTAGRAM_FIRST_URL}/${DEMO_SEARCH_ID}/followers`)
+  //await page.waitForXPath(`//*[@id="mount_0_0_je"]/div/div/div[3]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[2]/div[1]/div/div[1]/div`)
+  await page.waitForXPath(`//div/div/div[3]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[2]/div[1]/div/div[1]/div`)
+  //await page.waitForNavigation(waitOptions),
+      //await page.waitForSelector("#mount_0_0_6l > div > div > div:nth-child(3) > div > div > div.x9f619.x1n2onr6.x1ja2u2z > div > div.x1uvtmcs.x4k7w5x.x1h91t0o.x1beo9mf.xaigb6o.x12ejxvf.x3igimt.xarpa2k.xedcshv.x1lytzrv.x1t2pt76.x7ja8zs.x1n2onr6.x1qrby5j.x1jfb8zj > div > div > div > div > div.x7r02ix.xf1ldfh.x131esax.xdajt7p.xxfnqb6.xb88tzc.xw2csxc.x1odjw0f.x5fp0pe > div > div > div._aano > div:nth-child(1) > div > div:nth-child(1) > div")
+  /*
   await page.waitForTimeout(2000)
   const laterButton = await page.$x("//button[contains(., '後で')]");
   await (laterButton[0] as ElementHandle<Element>).click()
   await page.waitForTimeout(1000)
   await page.click("div.x9f619.x1n2onr6.x1ja2u2z > div > div > div > div.x78zum5.xdt5ytf.x10cihs4.x1t2pt76.x1n2onr6.x1ja2u2z > div.x9f619.xvbhtw8.x78zum5.x168nmei.x13lgxp2.x5pf9jr.xo71vjh.x1uhb9sk.x1plvlek.xryxfnj.x1c4vz4f.x2lah0s.x1q0g3np.xqjyukv.x1qjc9v5.x1oa3qoh.x1qughib > div.x9f619.xjbqb8w.x78zum5.x168nmei.x13lgxp2.x5pf9jr.xo71vjh.x1plvlek.xryxfnj.x1c4vz4f.x2lah0s.xdt5ytf.xqjyukv.x1qjc9v5.x1oa3qoh.x1nhvcw1.x1dr59a3.xixxii4.x13vifvy.xeq5yr9.x1n327nk > div > div > div > div > div.x1iyjqo2.xh8yej3 > div:nth-child(2) > span > div > a")
+  await page.waitForTimeout(500)
+  await page.type('input[aria-label="検索語句"]', 'iinoten')
+  */
   console.log("complete")
 
 
