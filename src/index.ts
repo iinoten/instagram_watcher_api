0763051e8258e015459740a23b1095838f110ee9
iinoten <nteten0313@icloud.com>
2023-07-09 10:17:33 +0900

    Add 実装アイデアのコメントを追加

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import { main } from './puppeteer';
 const app = express();
 const port = process.env.PORT || '8000';
 
+// ここでpuppeteerのブラウザを立ちあげちゃって(クラス生成しちゃって)あとは各ハンドラにオブジェクトを渡して使い回していく
+// ↑ リクエストが被った時にどういう挙動になるかわからない(後発のリクエストと被ってブラウザが変な挙動になりそう)
 app.get('/', main);
 app.get('/hello/:name', helloHandler);
 app.get('/check', testHandler);
